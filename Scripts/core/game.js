9667af92a3514a4d061df751f0f935dab89da018
Kevin Ma <kbma.kevin@gmail.com>
2016-10-15 22:58:05 -0400

    mouseover, mouseout events

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -13,11 +13,20 @@
         circle.y = stage.canvas.height / 2;
         circle.name = 'Blue Circle';
         stage.addChild(circle);
+        // change the cursor when hovering over circle
+        circle.cursor = 'pointer';
         circle.on('dblclick', function (e) { alert(e.target + ' was double clicked!'); });
+        circle.on('mouseover', function (e) {
+            this.alpha = 1;
+        });
+        circle.on('mouseout', function (e) {
+            this.alpha = .5;
+        });
     }
     function setupStage() {
         stage = new createjs.Stage(document.getElementById('canvas'));
         createjs.Ticker.framerate = 60;
+        stage.enableMouseOver();
         createjs.Ticker.on('tick', function (e) {
             stage.update();
         });
